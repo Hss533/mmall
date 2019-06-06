9cf101845af2ba7f46bd320b94c25a145fc75e12
hss <1097260937@qq.com>
2019-06-06 09:38:53 +0800

    ftp

--- src/main/java/com/mmall/util/FTPUtil.java
+++ src/main/java/com/mmall/util/FTPUtil.java
@@ -76,7 +76,7 @@ public class FTPUtil {
     public static boolean uploadFile(List<File> fileList) throws IOException
     {
         FTPUtil ftpUtil=new FTPUtil(ftpIp,21,ftpUser,ftpPass);
-        logger.info("开始连接ftp服务器");
+        logger.info("开始连接ftp服务器hss");
         logger.info("filelist是{}",fileList);
         boolean result=ftpUtil.uploadFile("img",fileList);
         logger.info("开始连接ftp服务器，结束上传，上传结果{}",result);
